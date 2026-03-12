373fb13a7ea71f07f5cdc93735612098b1e06ac5
Bhumi424 <bhumikumari424@gmail.com>
2026-03-12 22:10:16 +0530

    UC2:Basic Room Types & Static Availability

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,82 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+// Version 2.1
+// Use Case 2: Basic Room Types & Static Availability
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+// Abstract class representing a general Room
+abstract class Room {
+
+    protected String roomType;
+    protected int beds;
+    protected double price;
+
+    // Constructor
+    public Room(String roomType, int beds, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    // Method to display room details
+    public void displayRoomDetails() {
+        System.out.println("Room Type : " + roomType);
+        System.out.println("Beds      : " + beds);
+        System.out.println("Price     : ₹" + price);
+    }
+}
+
+// Single Room class
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 2000);
+    }
+}
+
+// Double Room class
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 3500);
+    }
+}
+
+// Suite Room class
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super("Suite Room", 3, 6000);
+    }
+}
+
+// Main class (Application Entry Point)
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("===== Welcome to Book My Stay =====");
+
+        // Creating room objects (Polymorphism)
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        // Static availability variables
+        int singleRoomAvailable = 5;
+        int doubleRoomAvailable = 3;
+        int suiteRoomAvailable = 2;
+
+        System.out.println("\n--- Available Room Types ---\n");
+
+        single.displayRoomDetails();
+        System.out.println("Available : " + singleRoomAvailable);
+        System.out.println();
+
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available : " + doubleRoomAvailable);
+        System.out.println();
+
+        suite.displayRoomDetails();
+        System.out.println("Available : " + suiteRoomAvailable);
+
+        System.out.println("\nThank you for using Book My Stay!");
     }
 }
